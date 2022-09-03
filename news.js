54af57dc2867ceada55050d090b30e371fd4751c
Tonmoy-Roy <emonismail69@gmail.com>
2022-09-03 12:05:59 +0600

    news heading done

--- news.js
+++ news.js
@@ -1,47 +1,53 @@
-// const newsTitleLoad=()=>{
-//     fetch('https://openapi.programming-hero.com/api/news/categories')
-//     .then(res=>res.json())
-//     .then(data =>newsTitleShow(data.data.news_catagory))
-// }
-// const newsTitleShow= (newsTitles) =>{
-//     const newsTitleContainers=document.getElementById('news-title-container');
-//     const uniqueArray = [];
-//     newsTitles.forEach(title =>{
-        
-//         const Div=document.createElement('div');
-//         Div.innerHTML=`
-//         <button onclick="titleload(${title.catagory_id})"class="btn mx-3 fw-bold" type="submit">
-//         ${title.catagory_name}</button>`;
-//         newsTitleContainers.appendChild(Div);
-//     });
-// }
-// const titleload=(titleId)=>{
-//     const url=`https://openapi.programming-hero.com/api/news/category/0${titleId}`;
-//     console.log(url);
-//     fetch(url) 
-//     .then (res =>res.json())
-//     .then(data =>comsol.log(data.data[0]))
-// }
-
-// newsTitleLoad();
 
 const loadTitle=()=>{
     fetch("https://openapi.programming-hero.com/api/news/categories")
     .then(res=>res.json())
-    .then(data=>displayTitle(data.data.news_category[0].category_name))
+    .then(data=>displayTitle(data.data.news_category))
 }
 const displayTitle=title=>{
-
+  
     const titleContainer=document.getElementById("news-title-container");
     for(const titleNews of title){
+       
         const titleDiv=document.createElement("div");
         titleDiv.innerHTML=`
-        <button onclick="titleload(${title.catagory_id})"class="btn mx-3 fw-bold" type="submit">
-        ${title.catagory_name}</button>`;
+        <button onclick="titleload(${titleNews.category_id})"class="btn mx-3 fw-bold " type="submit">
+        ${titleNews.category_name}</button>`;
 
          titleContainer.appendChild(titleDiv);
     }
 
 }
+  
+const titleload =(category_id)=>{
+    console.log(category_id)
+    fetch(`https://openapi.programming-hero.com/api/news/category/0${category_id}`)
+    .then(res=>res.json())
+    .then(data=>displayNews(data.data))
+}
+
+const displayNews=news=>{
+    const newsContainer = document.getElementById("title-click-news");
+    foundNews= forEach(news=>{
+        const newsDiv=document.createElement("div");
+        newsDiv.innerHTML=`
+        <div class="card card-side bg-white shadow-xl  flex flex-col md:flex-row">
+        <figure><img src="${data[0].category_id.thumbnail_url}" alt="Movie"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Watch</button>
+          </div>
+        </div>
+      </div>
+        `
+        newsContainer.appendChild(newsDiv);
+
+    })
+}
+
+
+
 
  loadTitle();
